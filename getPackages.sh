7435e5c1b84e4f0d562268bd6d0f93d21dc1a8ec
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2017-12-04 22:07:35 +0100

    Code simplification

--- getPackages.sh
+++ getPackages.sh
@@ -12,8 +12,10 @@ tar -xvzf libffi-3.2.1.tar.gz
 rm libffi-3.2.1.tar.gz
 echo "Applying patch to libffi-3.2.1"
 (cd libffi-3.2.1 ; patch -p 1 < ../libffi-3.2.1_patch ; cd ..)
-echo "Applying patch to Python-2.7.13"
-(cd Python-2.7.13 ; patch -p 1 < ../python2.7.13_ios_patch ; cd ..)
-echo "All done. Now open both Xcode files and compile"
+echo "Applying patches to Python-2.7.13"
+(cd Python-2.7.13 ; patch -p 1 < ../Python_Include.patch ; patch -p 1 < ../Python_Lib.patch ; patch -p 1 < ../Python_Modules.patch; patch -p 1 < ../Python_Parser.patch ; patch -p 1 < ../Python_Python.patch; patch -p 1 < ../Python_setup.patch ; cd ..)
+
+echo "All done. Now open libffi-3.2.1/libffi.xcodeproj and compile,\n
+move libffi.a to this directory, open Python_ios.xcodeproj and compile."
  
 
