1075d96c903276508e178e62836f5d9cf0fe28bc
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-02-11 18:00:09 +0100

    Moved to ios_system using dynamic libraries

--- getPackages.sh
+++ getPackages.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 
+HHROOT="https://github.com/holzschu"
+IOS_SYSTEM_VER="1.1"
+
 # Python-2.7.13
 echo "Downloading Python 2.7.13"
 curl -OL https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
 tar -xvzf Python-2.7.13.tgz
 rm Python-2.7.13.tgz
+echo "Downloading ios_system.framework"
+(cd Frameworks 
+curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/smallRelease.tar.gz
+( tar -xzf smallRelease.tar.gz --strip 1 && rm smallRelease.tar.gz ) || { echo "ios_system failed to download"; exit 1; }
+)
 echo "Downloading libffi-3.2.1" 
-curl -OL ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz 
+curl -OL https://www.mirrorservice.org/sites/sourceware.org/pub/libffi/libffi-3.2.1.tar.gz 
 tar -xvzf libffi-3.2.1.tar.gz
 rm libffi-3.2.1.tar.gz
 echo "Applying patch to libffi-3.2.1"
