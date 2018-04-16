6cd8d689e74858c1da94bc84ebf5348b24cbe5e4
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-04-16 12:56:34 +0200

    Automtic build of libffi

--- getPackages.sh
+++ getPackages.sh
@@ -20,12 +20,14 @@ echo "Downloading libffi-3.2.1"
 curl -OL https://www.mirrorservice.org/sites/sourceware.org/pub/libffi/libffi-3.2.1.tar.gz 
 tar -xvzf libffi-3.2.1.tar.gz
 rm libffi-3.2.1.tar.gz
-echo "Applying patch to libffi-3.2.1"
+echo "Applying patch to libffi-3.2.1:"
 (cd libffi-3.2.1 ; patch -p 1 < ../libffi-3.2.1_patch ; cd ..)
+echo "Compiling libffi-3.2.1:"
+(cd libffi-3.2.1 ;  xcodebuild -project libffi.xcodeproj -target libffi-iOS -sdk iphoneos -arch arm64 -configuration Debug -quiet
+; mv build/Debug-iphoneos/libffi.a .. ; cd ..)
 echo "Applying patches to Python-2.7.13"
 (cd Python-2.7.13 ; patch -p 1 < ../Python_Include.patch ; patch -p 1 < ../Python_Lib.patch ; patch -p 1 < ../Python_Modules.patch; patch -p 1 < ../Python_Parser.patch ; patch -p 1 < ../Python_Python.patch; patch -p 1 < ../Python_setup.patch ; cd ..)
 
-echo "All done. Now open libffi-3.2.1/libffi.xcodeproj and compile,\n
-move libffi.a to this directory, open Python_ios.xcodeproj and compile."
+echo "All done. Now open Python_ios.xcodeproj and compile."
  
 
